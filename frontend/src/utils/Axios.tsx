8780a6058d09e4d5ff4ef8c5f196336b4eb41bad
Abhay <abhay.cse02@gmail.com>
2025-01-03 11:22:38 +0530

    render deployment change

--- frontend/src/utils/Axios.tsx
+++ frontend/src/utils/Axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosClient = axios.create({
-  baseURL: "http://localhost:4000/api/v1/",
+  baseURL: "https://ai-code-generator-backend.onrender.com/api/v1/",
 });
 
 export { AxiosClient };
